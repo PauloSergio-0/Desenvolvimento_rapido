30c6d6fe74b5e0855797308576832866fb526980
AlunoTI <AlunoTI@estacio.acad>
2024-09-12 20:41:08 -0300

    add data

--- src/routes/file_route.py
+++ src/routes/file_route.py
@@ -11,14 +11,14 @@ async def create_file():
     return FileProcessor().create_file()
 
 
-@router.post("/upload_file.")
-async def upload_file( file: UploadFile = File(...)):
-    return await FileProcessor.upload_file(file)
+@router.post("/upload_file")
+async def upload_file(file: UploadFile = File(...)):
+    return await FileProcessor().upload_file(file)
 
 @router.post("/file/add_data")
-async def add_data():
-    return {"message": "Dado adicionado com sucesso"}
-
+async def add_data(conta: str, agencia: str, texto: str, valor: float):
+    data = {"conta": conta, "agencia": agencia, "texto": texto, "valor": valor}
+    return await FileProcessor().add_data_to_file(data)
 @router.post("/file/delete_data")
 async def delete_data():
     return {"message": "Dado deletado com sucesso"}
