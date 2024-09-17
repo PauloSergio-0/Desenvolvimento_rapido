49be27b625e779b1f0113dbbe88207b64e24a9ee
Paulo Sérgio <paulosergiosrs08@gmail.com>
2024-09-17 10:50:24 -0300

    Salvar mudanças antes de reescrever o histórico

--- src/routes/file_route.py
+++ src/routes/file_route.py
@@ -15,15 +15,18 @@ async def create_file():
 async def upload_file(file: UploadFile = File(...)):
     return await FileProcessor().upload_file(file)
 
+
 @router.post("/file/add_data")
 async def add_data(conta: str, agencia: str, texto: str, valor: float):
     data = {"conta": conta, "agencia": agencia, "texto": texto, "valor": valor}
     return await FileProcessor().add_data_to_file(data)
 
+
 @router.delete("/file/delete_data")
 async def delete_data():
     delete_file =  await FileProcessor().delete_file()
     return {"menssage": delete_file}
+
 
 @router.post("/file/list_files")
 async def list_files():
